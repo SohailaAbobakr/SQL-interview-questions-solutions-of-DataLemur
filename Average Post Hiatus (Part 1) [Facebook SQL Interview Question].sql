64ef9ec1e31ba8d8d009a7a160e854fa79a05cec
Sohaila Mahmoud <81999695+SohailaAbobakr@users.noreply.github.com>
2023-10-30 16:36:43 +0200

    Add files via upload

---
+++ Average Post Hiatus (Part 1) [Facebook SQL Interview Question].sql
@@ -0,0 +1,7 @@
+SELECT user_id, 
+       EXTRACT(DAY FROM (MAX(post_date)-MIN(post_date))) AS days_between
+FROM posts
+WHERE post_date BETWEEN '01/01/2021' AND '12/31/2021'
+GROUP BY user_id
+HAVING  COUNT(user_id) > 1
+;
